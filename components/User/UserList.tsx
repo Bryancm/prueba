0d20841c03a7392d302d495dfbfd184155edbc1c
Bryancm <bryan.mtzs@gmail.com>
2022-03-02 14:15:32 -0600

    search input layout

--- components/User/UserList.tsx
+++ components/User/UserList.tsx
@@ -1,4 +1,5 @@
 import type { FC } from "react";
+import SearchInput from "../Input/SearchInput";
 import UserItem from "./UserItem";
 
 const users = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]; // array para maquetar
@@ -7,6 +8,7 @@ const UserList: FC = () => {
   return (
     <div className="flex flex-col justify-center items-center">
       <h1 className="text-6xl font-bold m-4">People</h1>
+      <SearchInput />
       <div className="flex flex-row justify-center max-w-6xl flex-wrap p-4">
         {users.map((user) => (
           <UserItem key={user} />
