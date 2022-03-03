fac57b0ac26b61f696ed8c544429947682e4f3ac
Bryancm <bryan.mtzs@gmail.com>
2022-03-03 15:16:28 -0600

    user profile responsive

--- components/Post/PostItem.tsx
+++ components/Post/PostItem.tsx
@@ -7,7 +7,7 @@ const tags = ["animal", "dog", "golden retriever"]; //array para maquetar
 
 const PostItem: FC = () => {
   return (
-    <div className="flex flex-col border rounded-md w-3/4 m-4 pt-4 px-4 pb-2">
+    <div className="flex flex-col border rounded-md w-11/12 sm:w-3/4 m-4 pt-4 px-4 pb-2">
       <div className="flex flex-row justify-between items-center">
         <div className="flex flex-row items-center">
           <Image
@@ -18,16 +18,16 @@ const PostItem: FC = () => {
             height={60}
           />
           <div className="mx-2">
-            <h1 className="font-bold">Mr. Rudi Droste</h1>
-            <p className="text-gray-400 text-sm">60d0fe4f5311236168a109ce</p>
+            <h1 className="font-bold text-sm sm:text-md">Mr. Rudi Droste</h1>
+            <p className="text-gray-400 text-xs sm:text-sm">60d0fe4f5311236168a109ce</p>
           </div>
         </div>
-        <p className="text-gray-400 text-sm">May 24 2020 09:53:17</p>
+        <p className="text-gray-400 text-xs sm:text-sm text-right w-1/4">May 24 2020 09:53:17</p>
       </div>
       <p className="text-gray-700 py-4 leading-relaxed">Gratitude short-coated tan dog on seashore</p>
-      <div className="flex justify-center items-center p-2 relative h-96">
+      <div className="flex justify-center items-center p-2 relative h-80 sm:h-96">
         <Image
-          className="rounded"
+          className="rounded cursor-pointer"
           objectFit="cover"
           layout="fill"
           src="https://images.ctfassets.net/lh3zuq09vnm2/yBDals8aU8RWtb0xLnPkI/19b391bda8f43e16e64d40b55561e5cd/How_tracking_user_behavior_on_your_website_can_improve_customer_experience.png"
--- components/Tag.tsx
+++ components/Tag.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Tag: FC<Props> = ({ text }) => {
   return (
-    <button className="bg-slate-200 mr-2 mb-2 px-2 py-1 rounded text-gray-700 text-sm transition ease-in duration-200 hover:shadow-md">
+    <button className="bg-slate-200 mr-2 mb-2 px-2 py-1 rounded text-gray-700 text-xs sm:text-sm transition ease-in duration-200 hover:shadow-md">
       {text}
     </button>
   );
