c7c1f00cf9139cae72edfaa988321cc5a5bfc955
Bryancm <bryan.mtzs@gmail.com>
2022-03-02 19:33:31 -0600

    user profile layout

--- components/Input/SearchInput.tsx
+++ components/Input/SearchInput.tsx
@@ -1,13 +1,10 @@
 import type { FC } from "react";
+import { SearchIcon } from "@heroicons/react/outline";
 
 const SearchInput: FC = () => {
   return (
     <div className="m-4 w-1/2 relative">
-      <span className="absolute top-3 left-2 text-gray-400">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-          <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-        </svg>
-      </span>
+      <SearchIcon className="absolute top-3 left-2 h-6 w-6 text-gray-400" />
       <input placeholder="Search" className=" bg-slate-50 h-12 rounded-sm border shadow p-4 pl-10 w-full" />
     </div>
   );
--- components/User/UserItem.tsx
+++ components/User/UserItem.tsx
@@ -1,20 +1,24 @@
 import type { FC } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const UserItem: FC = () => {
   return (
-    <div className="flex flex-col justify-center items-center border rounded-md w-60 h-60 m-4">
-      <Image
-        className="rounded-full"
-        src="https://images.ctfassets.net/lh3zuq09vnm2/yBDals8aU8RWtb0xLnPkI/19b391bda8f43e16e64d40b55561e5cd/How_tracking_user_behavior_on_your_website_can_improve_customer_experience.png"
-        width={120}
-        height={120}
-      />
-      <div className="m-2 text-center">
-        <h1 className="font-bold m-1">Mr. Rudi Droste</h1>
-        <p className="text-gray-400 text-sm">60d0fe4f5311236168a109ce</p>
+    <Link href="/user/1">
+      <div className="flex flex-col justify-center items-center border rounded-md w-60 h-60 m-4 cursor-pointer">
+        <Image
+          className="rounded-full"
+          layout="fixed"
+          src="https://images.ctfassets.net/lh3zuq09vnm2/yBDals8aU8RWtb0xLnPkI/19b391bda8f43e16e64d40b55561e5cd/How_tracking_user_behavior_on_your_website_can_improve_customer_experience.png"
+          width={120}
+          height={120}
+        />
+        <div className="m-2 text-center">
+          <h1 className="font-bold m-1">Mr. Rudi Droste</h1>
+          <p className="text-gray-400 text-sm">60d0fe4f5311236168a109ce</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
