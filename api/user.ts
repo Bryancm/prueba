8ac6dc6f4187b24ade697ddfe023772eef84804e
Bryancm <bryan.mtzs@gmail.com>
2022-03-04 11:32:06 -0600

    url fix

--- api/user.ts
+++ api/user.ts
@@ -1,5 +1,5 @@
 import fetcher from "./fetch";
-const baseUrl = "https://dummyapi.io/data/v1/useryy";
+const baseUrl = "https://dummyapi.io/data/v1/user";
 
 export const fetchUsers = async () => {
   const res = await fetcher(`${baseUrl}?limit=12`);
